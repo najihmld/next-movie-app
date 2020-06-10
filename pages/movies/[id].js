8729a23e45796ba8e96559ebd615051a16ef609a
Najih Mld <najihmaulida@gmail.com>
2020-06-10 11:48:07 +0700

    get movie data by id

--- pages/movies/[id].js
+++ pages/movies/[id].js
@@ -1,13 +1,35 @@
 import { useRouter } from 'next/router'
+import { getMovieById } from '../../actions'
 
-const Movie = () => {
+const Movie = (props) => {
   const router = useRouter()
   const { id } = router.query
+  const { movie } = props
   return (
     <div className="container">
-      <h1>Moview with id: {id} </h1>
+      <div className="jumbotron">
+        <h1 className="display-4">{ movie.name }</h1>
+         <p className="lead">{movie.description}</p>
+        <hr className="my-4" />
+          <p>{movie.genre}</p>
+        <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+      </div>
+        <p className="desc-text">{movie.longDesc}</p>
+        <style jsx>{`
+          .desc-text {
+            font-size: 21px;
+          }
+        `}</style>
     </div>
   )
 }
+
+// call getMovieById
+
+Movie.getInitialProps = async ({ query }) => {
+  const movie = await getMovieById(query.id)
+  return { movie }
+}
+
 
 export default Movie
