b620c34dc855e44e8960ae95e1265aa984b887ba
Najih Mld <najihmaulida@gmail.com>
2020-06-22 09:53:53 +0700

    modal implementation + close modal

--- components/sideMenu.js
+++ components/sideMenu.js
@@ -1,9 +1,11 @@
 import {useState} from 'react'
+import Modal from './modal'
 
 const SideMenu = (props) => {
   const { categories } = props
   return (
     <div>
+      <Modal />
       <h1 className="my-4">{props.appName}</h1>
       <div className="list-group">
         {
