e5b25afe0d6103729876792fe7eb28ec8e4d22d3
Najih Mld <najihmaulida@gmail.com>
2020-06-22 14:57:22 +0700

    close modal & uncontrolled data functionality

--- components/sideMenu.js
+++ components/sideMenu.js
@@ -5,16 +5,18 @@ import { createMovie } from '../actions'
 
 const SideMenu = (props) => {
   const { categories } = props
+  let modal = null
 
   const handleCreateMovie = (movie) => {
     createMovie(movie).then((movies) => {
       console.log(JSON.stringify(movies))
+      modal.closeModal()
     })
   }
 
   return (
     <div>
-      <Modal hasSubmit={false}>
+      <Modal ref={ele => modal = ele} hasSubmit={false}>
         <MovieCreateForm handleFormSubmit={handleCreateMovie} />
       </Modal>
       <h1 className="my-4">{props.appName}</h1>
