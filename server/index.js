45c66c5aaf251f7e8a0e430a59c5b75e288c7f48
Najih Mld <najihmaulida@gmail.com>
2020-06-23 13:10:56 +0700

    get movie by ID

--- server/index.js
+++ server/index.js
@@ -17,16 +17,19 @@ app.prepare().then(() => {
   server.get('/api/v1/movies', (req, res) => {
     return res.json(moviesData)
   })
+  
+  server.get('/api/v1/movies/:id', (req, res) => {
+    const { id } = req.params
+
+    const movie = moviesData.find((m) => m.id === id)
+
+    return res.json(movie)
+  })
 
   server.post('/api/v1/movies', (req, res) => {
     const movie = req.body
     console.log(JSON.stringify(movie))
     return res.json({...movie, author: 'Najih Mld'})
-  })
-
-  server.patch('/api/v1/movies/:id', (req, res) => {
-    const { id } = req.params
-    return res.json({message: `Updating post of id: ${id}`})
   })
 
   server.delete('/api/v1/movies/:id', (req, res) => {
