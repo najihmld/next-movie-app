6d249f2c81d6bc2ad1973beb448e6f519514cbbc
Najih Mld <najihmaulida@gmail.com>
2020-06-23 10:29:02 +0700

    getting movies from server

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@
 
 const next = require('next')
 const express = require('express');
+const bodyParser = require('body-parser')
+const moviesData = require('./data.json')
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
@@ -10,6 +12,33 @@ const handle = app.getRequestHandler()
 app.prepare().then(() => {
 
   const server = express();
+  server.use(bodyParser.json())
+
+  server.get('/api/v1/movies', (req, res) => {
+    return res.json(moviesData)
+  })
+
+  server.post('/api/v1/movies', (req, res) => {
+    const movie = req.body
+    console.log(JSON.stringify(movie))
+    return res.json({...movie, author: 'Najih Mld'})
+  })
+
+  server.patch('/api/v1/movies/:id', (req, res) => {
+    const { id } = req.params
+    return res.json({message: `Updating post of id: ${id}`})
+  })
+
+  server.delete('/api/v1/movies/:id', (req, res) => {
+    const { id } = req.params
+    return res.json({message: `Deleting post of id: ${id}`})
+  })
+
+
+  // server.get('/faq', (req, res) => {
+  //   res.send(`<h1>Hello</h1>`)
+  // })
+
   // we are handling all of the request comming to our server
   server.get('*', (req, res) => {
     // next.js is handling request and providing pages where we are navigating to
