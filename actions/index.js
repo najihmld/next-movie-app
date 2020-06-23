79f05da14d10924044bf31a69bdd63058e07bac9
Najih Mld <najihmaulida@gmail.com>
2020-06-23 15:05:32 +0700

    delete movie

--- actions/index.js
+++ actions/index.js
@@ -61,3 +61,7 @@ export const getMovieById = (id) => {
   //   setTimeout(() =>  resolve(movie), 50)
   // })
 }
+
+export const deleteMovie = (id) => {
+  return axios.delete(`${BASE_URL}/api/v1/movies/${id}`).then(res => res.data)
+}
