306109bd022c41d49b5633fe2e7695591c7eb849
Najih Mld <najihmaulida@gmail.com>
2020-06-23 20:25:12 +0700

    provide data to edit page

--- components/movieCreateForm.js
+++ components/movieCreateForm.js
@@ -1,15 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 const MovieCreateForm = (props) => {
+  
+  const [ isInitialDataLoaded, setIsInitialDataLoaded ] = useState(false)
 
-  const [form, setForm] = useState({
+  const defaultData = {
     name: '',
     description: '',
     rating: '',
     image: '',
     cover: '',
     longDesc: '',
-  })
+  }
+
+  const formData = props.initialData ? {...props.initialData} : defaultData
+  const [form, setForm] = useState(formData)
+
+  // // for edit page
+  // useEffect(() => {
+  //   if (props.initialData) {
+  //     setForm(props.initialData)
+  //     setIsInitialDataLoaded(true)
+  //   }
+  // }, [isInitialDataLoaded] )
+  // //
 
   const handleChange = (event) => {
     const target = event.target
--- pages/movies/[id]/edit.js
+++ pages/movies/[id]/edit.js
@@ -4,27 +4,40 @@ import { getMovieById } from '../../../actions'
 
 class EditMovie extends React.Component {
 
-  static getInitialProps({query}) {
-    return {query}
+  // static getInitialProps({query}) {
+  //   return {query}
+  // }
+
+  static async getInitialProps({query}) {
+    const movie = await getMovieById(query.id)
+
+    return { movie }
   }
 
-  state = {
-    movie: {}
-  }
+  // state = {
+  //   movie: {
+  //     name: '',
+  //     description: '',
+  //     rating: '',
+  //     image: '',
+  //     cover: '',
+  //     longDesc: '',
+  //   }
+  // }
 
-  componentDidMount() {
-    const { id } = this.props.query
-    getMovieById(id).then(movie => {
-      this.setState({movie})
-    })
-  }
+  // componentDidMount() {
+  //   const { id } = this.props.query
+  //   getMovieById(id).then(movie => {
+  //     this.setState({movie})
+  //   })
+  // }
 
   render() {
+    const { movie } = this.props
     return (
       <div className="container">
         <h1>Edit Movie</h1>
-        { JSON.stringify(this.state.movie) }
-        <MovieCreateForm />
+        <MovieCreateForm initialData={movie} />
       </div>
     )
   }
